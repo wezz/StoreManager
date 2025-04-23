2590bd108aa5ea968c995357a5a116cbb5487ce4
Johan Tor <johan.tor@gmail.com>
2025-04-23 13:35:33 +0200

    fix: build solution

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,13 @@
-export { default as StoreManager } from './StoreManager';
+export declare class StoreManager {
+    private prefix;
+    constructor(_prefix?: string);
+    private getStorageMedium;
+    Has(key: string): boolean;
+    Get(key: string): any;
+    private toJSONIfJSON;
+    Save(key: string, data: any, permanent?: boolean): void;
+    Set(key: string, data: any, permanent?: boolean): boolean;
+    Remove(key: string): void;
+}
+
+export { }
